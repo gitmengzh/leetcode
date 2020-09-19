40e071c1fb58de31acc0fd659fc4e6b1cb07d0f2
zhenweicui <18604287698@163.com>
2020-09-19 14:00:44 +0800

    integer to toman udpate

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -30,31 +30,41 @@ M             1000
 解释: M = 1000, CM = 900, XC = 90, IV = 4.
 链接：https://leetcode-cn.com/problems/integer-to-roman
 '''
-
-def integer_to_number(nums):
+# 思路： 将数字转化程字符串，然后从最后一位开始遍历，0不考虑，如果数字在字典中有对应值，则返回对应值且乘系数，
+# 设置系数i=0，每次循环过后加1，10的i次方为系数
+# 分别对4，9，【2，3】， 【6，7，8】进行处理
+def integer_to_number(num):
     r_dict = {'1':'I', '5':'V', '10':'X', '50':'L', '100':'C', '500':'D', '1000':'M'}
-    roman = [None]
-    str_nums = str(nums)
+    roman = []
+    str_nums = str(num)
     i = 0
-    # for i in range(len(str_nums)):
-    for j in str_nums:
+    for j in str_nums[::-1]:
         if str(int(j)*(10**i)) in r_dict.keys():
-            roman[0] = r_dict.get(str(int(j)*10**i))
-        elif j =='4':
-            roman[0] = r_dict.get(str(1*10**i))+r_dict.get(str(5*10**i))
-        elif j=='9':
-            roman[0] = r_dict.get(str(1*10**i))+r_dict.get(str(10**(i+1)))
-        elif j in ['1','2','3']:
-            roman[0] = (r_dict.get(str(10**i)))*j
-        else:
-            roman[0] = r_dict.get(str(5*10**i))+(r_dict.get(str(10**i)))*(int(j)-5)
+            roman.append(r_dict.get(str(int(j)*10**i)))
+        elif j == '4':
+            roman.append(r_dict.get(str(1*10**i))+r_dict.get(str(5*10**i)))
+        elif j == '9':
+            roman.append(r_dict.get(str(1*10**i))+r_dict.get(str(10**(i+1))))
+        elif j in ['2','3']:
+            roman.append((r_dict.get(str(10**i)))*int(j))
+        elif j in ['6','7','8']:
+            roman.append(r_dict.get(str(5*10**i))+(r_dict.get(str(10**i)))*(int(j)-5))
         i=i+1
-        roman.insert(0,None)
+
+    result = "".join(roman[::-1])
+    return result
 
 
+def integer_to_number2(num):
+    d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V',
+         4: 'IV', 1: 'I'}
+    result = ''
+    for i in d:
+        while num > i:
+            num = num-i
+            result = result + d.get(i)
+    return result
 
-
-    return roman
 
 
 
@@ -67,6 +77,7 @@ if __name__ == "__main__":
     num3 = 9
     num4 = 58
     num5 = 1994
+    num6 = 44
     print(integer_to_number(num5))
    #  a = [1]
    #  a.insert(0,1)
