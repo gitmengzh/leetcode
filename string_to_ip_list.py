b9d080f8b95d6c2ef5e2f6dfd62624885d53a9c6
mengzh <mengzh1618@gmail.com>
2020-10-19 23:46:19 +0800

    update code

--- string_to_ip_list.py
+++ string_to_ip_list.py
@@ -1,30 +1,21 @@
 '''
 要求：返回一个 IP 数组，并且按 IP 最后一位排序返回
 string = "192.0.0.1？！289.0.0.1！0.0.0.0！192.163.10.28？192.0.0.1”
+此题我有一个疑问，IP最后一位只的是4段ip中的最后一位，还是字符串最后一位？
+暂时理解成字符串最后一位吧，可能我的第一种理解不正确
 '''
 line = "192.0.0.1?!289.0.0.1!0.0.0.0!20.163.10.28?192.0.0.9"
 import re
 import operator
 res = re.split(r'[?!]+', line)
-# L.sort(key=operator.itemgetter())
-
-
-# res.sort(key=operator.itemgetter())
-res.sort(key=lambda x:x.split('.')[-1])
+# 方法一使用lambda
 # res.sort(key=lambda x:x[-1])
-# print(res)
-# a  = '192.163.10.28'
-s = []
-a = ['192.0.0.1', '211.0.0.29', '0.0.0.291', '192.163.10.8', '192.0.0.1']
+# 方法2 使用operator.itemgetter
+res.sort(key=operator.itemgetter(-1))
 
-for i in a:
-    j = lambda a:a.split('.')[-1]
-    s.append(j(i))
-# b = lambda x:a[-2].split('.')[-1]
-# a.sort(key=lambda x:int(x.split('.')[-1]))
-print(sorted(s,key=lambda x:x[-1]))
-['192.0.0.1', '289.0.0.1', '0.0.0.0', '192.163.10.28', '192.0.0.1']
-['0.0.0.0', '192.0.0.1', '289.0.0.1', '192.0.0.1', '192.163.10.28']
-['1', '29', '291', '28', '1']
-['192.0.0.1', '192.0.0.1', '192.163.10.28', '0.0.0.291', '211.0.0.9']
-## 注意字符串和数字排序的区别
+## 注意字符串和数字排序的区别, 注意这里排序的是按照字符串最后一位排序
+# sort和sorted都可以这么使用
+# 如果是按照ip地址四段中的最后一段排序，那么代码如下：
+# res.sort(key= lambda x:int(x.split('.')[-1]))
+
+print(res)
