c4bb083f41aecf5f4c62b966b70739de53b29925
mengzh <mengzh1618@gmail.com>
2021-03-17 00:08:38 +0800

    create string and update reverse string

---
+++ _string/reverse_string_1.py
@@ -0,0 +1,32 @@
+'''
+将字符串中的单词进行反转，然后输出
+eg:
+    input:'I am a Student.'
+    output:'student. a am I'
+'''
+
+
+class Solution(object):
+
+    @staticmethod
+    def reverse_string(s: str):
+        new_s = s.strip()  # 去掉收尾空格
+        new_list = new_s.split(' ')
+        res = ' '.join(new_list[::-1])
+        return res
+
+    @staticmethod
+    def reverse_string2(s: str):  # 将每个字符串反转，再整体反转
+        new_s = s.strip()
+        for i in new_s:
+            reversed(i)
+        new_list = new_s.split(' ')
+        new_list.reverse()
+        return ' '.join(new_list)
+
+
+
+if __name__ == "__main__":
+    s1 = ' I am a Student.  '
+    s2 = ''
+    print(Solution.reverse_string2(s1))
