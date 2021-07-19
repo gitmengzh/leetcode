7abe4e6e872e9be2d6f3bc23d2571b5743da5eef
mengzh <mengzh1618@gmail.com>
2021-07-19 19:11:31 +0800

    add leetcode_5161, 5814, update leetcode_132

--- Array/leetcode-132.py
+++ Array/leetcode-132.py
@@ -21,41 +21,9 @@ def solution1(nums):        # 使用sum
 def solution2(nums):        # 异或
     for i in range(nums):
         pass
-# 5161. 可以输入的最大单词数
-def canBeTypedWords(text, brokenLetters) :
-        text_list = text.split(' ')
-        res = 0
-        for i in text_list:
-            for j in i:
-                if j in brokenLetters:
-                    break
-            else:
-                res += 1
-        return res
-
-# 5814. 新增的最少台阶数
-def addRungs(rungs, dist):
-
-    dst = 1
-    res = 0
-    if rungs[0] > dist:
-        if rungs[0] % dist == 0:
-            res += rungs[0]//dist - 1
-        else:
-            res = rungs[0]//dist
 
 
 
-    while dst < len(rungs):
-        if rungs[dst] - rungs[dst - 1] > dist:
-            a = (rungs[dst] - rungs[dst - 1])
-            b = a//dist
-            if a % dist == 0:
-                res += b - 1
-            else:
-                res += b
-        dst += 1
-    return res
 
 
 if __name__ == "__main__":
