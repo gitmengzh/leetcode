444df946abb0763742dafec5d0784a527ab1e59f
mengzh <mengzh1618@gmail.com>
2021-07-22 20:54:22 +0800

    update  leetcode_1

--- Array/leetcode_1.py
+++ Array/leetcode_1.py
@@ -27,3 +27,30 @@
     进阶：你可以想出一个时间复杂度小于O(n2)的算法吗？
     链接：https://leetcode-cn.com/problems/two-sum/
 """
+
+
+def twoSum1(nums, target):   # 使用python list index方法
+
+    for i in range(len(nums) - 1):
+
+        if (target - nums[i]) in nums[i+1:]:
+            return [i, nums[i+1:].index(target-nums[i])+i+1]
+
+def twoSum2(nums, target):  # hashmap
+    hashtable = {}
+    for i, num in enumerate(nums):      # enumerate()将nums的index和对应的元素组成一个字典（待确定是不是字典类型）
+        if (target-num) in hashtable:
+            print(hashtable)
+            return [hashtable[target-num], i]
+
+        hashtable[num] = i
+
+
+
+if __name__ == "__main__":
+    nums1 = []
+    nums2 = [1]
+    nums3 = [1,1]
+    nums4 = [1,2,2,1,2]
+    target = 2
+    print(twoSum2(nums4, target))
