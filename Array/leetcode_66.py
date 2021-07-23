357e4d53d6ec877e890805ee172be89050cc89fb
mengzh <mengzh1618@gmail.com>
2021-07-23 20:56:17 +0800

    update  leetcode_66

--- Array/leetcode_66.py
+++ Array/leetcode_66.py
@@ -23,3 +23,53 @@
 
 链接：https://leetcode-cn.com/problems/plus-one
 """
+
+
+def plusOne(digits) :
+    temp = digits[::-1]
+
+    if temp[0] + 1 > 9:
+        counter = 1
+        temp[0] = 0
+    else:
+        temp[0] += 1
+        return temp[::-1]
+    for i in range(1, len(temp)):
+        if temp[i] + counter > 9:
+            temp[i] = 0
+            counter = 1
+        else:
+            temp[i] += counter
+            counter = 0
+            break
+    if counter == 1:
+        temp.append(1)
+    return temp[::-1]
+
+    # count = 1
+    # for i in range(len(digits) - 1, -1, -1):
+    #     count, digits[i] = divmod(digits[i] + count, 10)
+    #     if count == 0:
+    #         break
+    # if count:
+    #     digits.insert(0,count)
+    # return digits
+
+
+def plusOne2(digits):
+    # for i in range(len(digits):
+    #     res = ''
+    #     res += str(digits[::-1][i]*)
+    # return res
+    # # return str(int(res)+1).split()
+    num = int(''.join([str(s) for s in digits])) + 1
+    return [int(s) for s in str(num)]
+
+
+
+
+if __name__ == "__main__":
+    digits1 = [1, 2, 4]
+    digits2 = [1, 9, 9]
+    digits3 = [9, 9, 9]
+    print(plusOne2(digits3))
