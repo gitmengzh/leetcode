966f8d28cb2af8056fb4e0c8da45b46487484e97
mengzh <mengzh1618@gmail.com>
2021-09-23 20:57:39 +0800

    update maximum_subarray

--- Dynamic/maximum_subarray.py
+++ Dynamic/maximum_subarray.py
@@ -27,23 +27,48 @@
 
 
 """
+# 动态规划：解决最优解问题
 
 def maxSubArray(nums):
     lenth = len(nums)
-    res = [0]*2
+    # res = [0]*lenth
     if lenth == 1:
         return nums[0]
     else:
-        res[0] = nums[0]
-        res[1] = max(nums[1], nums[1]+nums[0])
-        res = max(res[0], res[1])
-        temp = 0
+        res1 = nums[0]          # 用于保存上一个最大值
+        maxres = res1           # 用于保存最大值
+        for i in range(1,lenth):
+            res2 = max(nums[i], res1+nums[i])
+            if res2 > maxres:
+                maxres = res2
+            res1 = res2         # 替换上一个最大值，用于下一次循环
 
-        for i in range(2,lenth):
-            if res[i-1]+nums[i] >= res[i-1]:
-                res = res[i-1]+nums[i]
+        return maxres
+
+
+    """
+    if len(nums) == 1:
+            return nums[0]
+        if not nums:
+            return 0
+        a = nums[0]
+        b = a 
+        for i in range(1,len(nums)):
+            if a+nums[i] > nums[i]:
+                b = max(b, a+nums[i])
+                a = a+nums[i]
             else:
-                pass
+                b = max(a,b,a+nums[i], nums[i])
+                a = nums[i]
+        return b
+    """
 
-
+def maxSubArray1(nums):
+    for i in range(1, nums):
+        nums[i] = max(nums[i], nums[i]+nums[i-1])
+    return max(nums)
+if __name__ == "__main__":
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    nums1 = [-1,-2]
+    print(maxSubArray(nums))
 
